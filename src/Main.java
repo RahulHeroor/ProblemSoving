89b8fc61784188aae4f609ab8096b563d8b39600
rahulheroor <rahulheroor1@gmail.com>
2023-08-07 18:42:04 +0530

    my first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner obj=new Scanner(System.in);
+        System.out.println("enter the test cases");
+        int t=obj.nextInt();
+        for (int i=0; i<t; i++){
+            int n=obj.nextInt();
+            System.out.println(-n);
+        }
     }
+
 }
